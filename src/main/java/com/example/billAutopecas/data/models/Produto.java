fd2c3887294adb8f1326ffea73654a2c8969f784
daniels010 <danielkc27@gmail.com>
2024-08-25 11:51:29 -0300

    data class fix

--- src/main/java/com/example/billAutopecas/data/models/Produto.java
+++ src/main/java/com/example/billAutopecas/data/models/Produto.java
@@ -8,4 +8,51 @@ public class Produto {
     private float valorSaida;
     private int quantidadeAtual;
 
+    public void setCodigo(String codigo){
+        this.codigo = codigo;
+    }
+
+    public String getCodigo(){
+        return codigo;
+    }
+
+    public void setDescricao(String descricao){
+        this.descricao = descricao;
+    }
+
+    public String getDescricao(){
+        return descricao;
+    }
+
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+
+    public String getMarca(){
+        return marca;
+    }
+
+    public void setValorEntrada(float valorEntrada){
+        this.valorEntrada = valorEntrada;
+    }
+
+    public float getValorEntrada(){
+        return valorEntrada;
+    }
+
+    public void setValorSaida(float valorSaida){
+        this.valorSaida = valorSaida;
+    }
+
+    public float getValorSaida(){
+        return valorSaida;
+    }
+
+    public void setQuantidadeAtual(int quantidadeAtual){
+        this.quantidadeAtual = quantidadeAtual;
+    }
+
+    public int getQuantidadeAtual(){
+        return quantidadeAtual;
+    }
 }
